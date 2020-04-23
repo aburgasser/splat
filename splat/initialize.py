286362afd8a7717793aff951fee52adb25698c5d
Adam Burgasser <aburgasser@gmail.com>
2020-04-23 10:42:18 -0700

    Update initialize.py

--- splat/initialize.py
+++ splat/initialize.py
@@ -100,7 +100,7 @@ DEFAULT_CROSSSECTION_UNIT = u.cm**2
 DEFAULT_INSTRUMENT = 'SPEX-PRISM'
 
 # OLD SPECTRAL LIBRARY PARAMETERS
-DATA_FOLDER = '/resources/Spectra/'
+DATA_FOLDER = '/resources/Data/Public/'
 DB_SPECTRA_FILE = 'spectral_data.txt'
 DB_SOURCES_FILE = 'source_data.txt'
 
