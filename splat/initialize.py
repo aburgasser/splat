27c177ba28faecf22ced37d4d00a29de55cae1cc
Adam Burgasser <aburgasser@gmail.com>
2021-07-27 15:16:27 -0700

    Update initialize.py

--- splat/initialize.py
+++ splat/initialize.py
@@ -14,7 +14,7 @@ from astropy import units as u
 
 
 # program constants
-VERSION = '2021.07.22'
+VERSION = '2021.07.27'
 __version__ = VERSION
 SPLAT_URL = 'http://splat.physics.ucsd.edu/splat/'
 DOCUMENTATION_URL = 'http://pono.ucsd.edu/~adam/splat/'
