dd934a3b87be974286a3e35e26556aa328687c9c
Adam Burgasser <aburgasser@gmail.com>
2025-07-29 19:35:54 -0700

    fixing tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,12 @@
 """
 This subpackage contains modules and packages for testing the SPLAT code
 """
+import numpy
+import os
+import copy
+import glob
+from astropy import units as u            # standard units
+from astropy import constants as const        # physical constants in SI units
+from astropy import coordinates as coord      # coordinate conversion
+from astropy.io import fits
+from numpy.testing import assert_allclose
