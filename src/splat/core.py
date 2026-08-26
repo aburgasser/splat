daba4cb5b7b191196bb1d9a8f129174753e2e52d
ctheissen <ctheissen@gmail.com>
2026-08-26 15:33:05 -0700

    Small fixes to Allers & Liu fit statistics for J-type and K-type

--- src/splat/core.py
+++ src/splat/core.py
@@ -6315,13 +6315,13 @@ def classifyByIndex(sp,ref='burgasser',string_flag=True,round_flag=False,remeasu
             coeffs['jtype'] = {}
             coeffs['ktype'] = {}
             # Calculate The J and K spectral types for Allers method
-            jtype = classifyByStandard(sp, std_class='dwarf', fit_ranges=[1.07,1.40]) 
+            jtype = classifyByStandard(sp, std_class='dwarf', fit_ranges=[1.07,1.40], statistic='absdev') 
             #print('JType', jtype)
             #print(typeToNum(jtype[0]))
             coeffs['jtype']['spt']    = typeToNum(jtype[0])
             coeffs['jtype']['sptunc'] = 1.
             coeffs['jtype']['mask']   = 1.
-            ktype = classifyByStandard(sp, std_class='dwarf', fit_ranges=[1.90,2.20]) 
+            ktype = classifyByStandard(sp, std_class='dwarf', fit_ranges=[1.90,2.20], statistic='absdev') 
             #print('KType', ktype)
             #print(typeToNum(ktype[0]))
             coeffs['ktype']['spt']    = typeToNum(ktype[0])
